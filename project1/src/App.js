7f3437ec9ac9dd304c5d7f82943f1680e0b0c0b8
Arturas Klinga <arturas.klinga@panko.lt>
2021-11-28 15:55:57 +0200

    Creating reducer and actions.

--- project1/src/App.js
+++ project1/src/App.js
@@ -33,16 +33,7 @@ const App = () => {
             process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
         setRepos(res.data);
         setLoading(false);
-    };
-
-    // Search GitHub users.
-    const searchUsers = async text => {
-        setLoading(true);
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        setUsers(res.data.items);
-        setLoading(false);
-    };
+    }; 
 
     // Clear users from state.
     const clearUsers = () => {
@@ -67,7 +58,6 @@ const App = () => {
                             <Route exact path="/" render={props => (
                                 <Fragment>
                                     <Search 
-                                        searchUsers={searchUsers} 
                                         clearUsers={clearUsers} 
                                         showClear={users.length > 0 ? true : false}
                                         setAlert={showAlert} 
