2e888015d14e260c4d48dc0e78b64b45c3f6ef78
Arturas Klinga <arturas.klinga@panko.lt>
2022-02-14 22:45:55 +0200

    Ability to add new log to database.

--- project3/src/components/logs/AddLogModal.js
+++ project3/src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
     const [message, setMessage] = useState('');
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState('');
@@ -10,7 +13,16 @@ const AddLogModal = () => {
         if (message === '' || tech === '') {
             M.toast({ html: 'Please enter a message and tech.' })
         } else {
-            console.log(message, tech, attention);
+            const newLog = {
+                message,
+                attention,
+                tech,
+                date: new Date()
+            };
+
+            addLog(newLog);
+
+            M.toast({ html: `Log added by ${tech}`});
 
             // Clear fields.
             setMessage('');
@@ -57,15 +69,19 @@ const AddLogModal = () => {
                 </div>
             </div>
             <div className="modal-footer">
-                <a href="#!" onClick={onSubmit} className="model-close waves-effect blue waves-green btn">Enter</a>
+                <a href="#!" onClick={onSubmit} className="modal-close waves-effect blue waves-green btn">Enter</a>
             </div>
         </div>
     )
 };
 
+AddLogModal.propTypes = {
+    addLog: PropTypes.func.isRequired
+}
+
 const modalStyle = {
     width: '75%',
     height: "75%"
-}
+};
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
