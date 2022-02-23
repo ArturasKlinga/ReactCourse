2e14b734adfb821460db22b5844b58f27f8af737
Arturas Klinga <arturas.klinga@panko.lt>
2022-02-23 19:38:13 +0200

    Saving added new tech in a database.

--- project3/src/components/techs/AddTechModal.js
+++ project3/src/components/techs/AddTechModal.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
 
     const onSubmit = () => {
         if (firstName === '' || lastName === '') {
-            M.toast({ html: 'Please enter the first and last name.' })
+            M.toast({ html: 'Please enter the first and last name.' });
         } else {
-            console.log(firstName, lastName);
+            addTech({ firstName, lastName });
+            M.toast({ html: `${firstName} ${lastName} was added as a tech.` });
 
             // Clear fields.
             setFirstName('');
@@ -41,10 +45,14 @@ const AddTechModal = () => {
                 
             </div>
             <div className="modal-footer">
-                <a href="#!" onClick={onSubmit} className="model-close waves-effect blue waves-green btn">Enter</a>
+                <a href="#!" onClick={onSubmit} className="modal-close waves-effect blue waves-green btn">Enter</a>
             </div>
         </div>
     )
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+    addTech: PropTypes.func.isRequired
+};
+
+export default connect(null, { addTech })(AddTechModal);
